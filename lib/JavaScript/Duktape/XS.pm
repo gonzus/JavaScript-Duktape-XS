a71410ac097c9d60105221c3aa176ccbd3227d20
Gonzalo Diethelm <gonzalo.diethelm@booking.com>
2018-04-23 12:29:02 +0200

    Bump version

--- lib/JavaScript/Duktape/XS.pm
+++ lib/JavaScript/Duktape/XS.pm
@@ -6,7 +6,7 @@ use warnings;
 use XSLoader;
 use parent 'Exporter';
 
-our $VERSION = '0.000048';
+our $VERSION = '0.000049';
 XSLoader::load( __PACKAGE__, $VERSION );
 
 our @EXPORT_OK = qw[];
@@ -26,7 +26,7 @@ engine
 
 =head1 VERSION
 
-Version 0.000048
+Version 0.000049
 
 =head1 SYNOPSIS
 
