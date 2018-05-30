70e01a44091af850a08861ec029d35838de8f270
Gonzalo Diethelm <gonzalo.diethelm@booking.com>
2018-05-30 10:25:09 +0200

    Use duk_push_sprintf and avoid a fixed-size buffer

--- pl_eventloop.c
+++ pl_eventloop.c
@@ -13,17 +13,16 @@ int pl_register_eventloop(Duk* duk)
 int pl_run_function_in_event_loop(Duk* duk, const char* func)
 {
     duk_context* ctx = duk->ctx;
+    duk_int_t rc = 0;
 
     // Start a zero timer which will call our function from the event loop.
-    duk_int_t rc = 0;
-    char js[256];
-    int len = sprintf(js, "setTimeout(function() { %s(); }, 0);", func);
-    rc = duk_peval_lstring(ctx, js, len);
+    duk_push_sprintf(ctx, "setTimeout(function() { %s(); }, 0);", func);
+    rc = duk_peval(ctx);
     if (rc != DUK_EXEC_SUCCESS) {
-        croak("Could not eval JS event loop dispatcher %*.*s: %d - %s\n",
-              len, len, js, rc, duk_safe_to_string(ctx, -1));
+        croak("Could not eval JS event loop dispatcher for %s: %d - %s\n",
+              func, rc, duk_safe_to_string(ctx, -1));
     }
-    duk_pop(ctx);
+    duk_pop(ctx); // pop result / error
 
     // Launch eventloop; this call only returns after the eventloop terminates.
     rc = duk_safe_call(ctx, eventloop_run, duk, 0 /*nargs*/, 1 /*nrets*/);
